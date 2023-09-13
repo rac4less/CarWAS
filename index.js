c4d45ad965b28d4be763e74138b55f9867ed2332
rac4less <142186642+rac4less@users.noreply.github.com>
2023-09-13 15:26:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.post('/send-email', async (req, res) => {
                 subject: `Car Walk Around from ${fullName}`,
                 html: `<h1>Walk Around 4less</h1>
       <p>City: ${city} | Region: ${region} | Latitude and longitude: ${latitude},${longitude} (paste it on Google Maps) | The following information was submitted by ${fullName} on ${new Date()} with email: ${email} using Walk around 4less: a website to easily send the information about the rented vehicle before using it. 
-      By sending this email, the user guarantees that he accepts the <a href="https://drive.google.com/file/d/1qUzcF3mbIv1VS_lODQLAFWSKohfGUXw0/view" target="_blank" rel="noopener noreferrer">terms and conditions</a> in which the vehicle is located, and also provides evidence of the car before using it.</p>`,
+      By sending this email, the user guarantees that he accepts the <a href="https://drive.google.com/file/d/1LoJia2xn7Jufgd7H4MkftmXZa2ptSiQ6/view?usp=sharing" target="_blank" rel="noopener noreferrer">terms and conditions</a> in which the vehicle is located, and also provides evidence of the car before using it.</p>`,
                 attachments,
             },
             (err, info) => {
